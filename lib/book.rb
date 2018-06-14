99805fce4fc1a5591488fbeb7e527dd2d78e31b9
Jason Mei <jason.mei403@gmail.com>
2018-06-14 03:48:35 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,5 @@
 class Book
+  attr_accessor :author :page_count :genre
   def initialize(title)
     @title = title
   end
@@ -6,13 +7,13 @@ class Book
   def title
     @title
   end
-
-  def author=(author)
-    @author = author
-  end
-
-  def author
-    @author
-  end
+  #
+  # def author=(author)
+  #   @author = author
+  # end
+  #
+  # def author
+  #   @author
+  # end
 
 end
